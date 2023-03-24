1f516f895a5c417b77be442143a2d85f516decc5
José Luis González <joseluisgonsan@gmail.com>
2023-03-24 18:14:00 +0100

    Postman

--- Modulo06/mynotes-server/src/main/kotlin/joseluisgs/es/models/Note.kt
+++ Modulo06/mynotes-server/src/main/kotlin/joseluisgs/es/models/Note.kt
@@ -1,15 +1,16 @@
 package joseluisgs.es.models
 
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Note(
-    val id: Long,
+    val id: Long = -1,
     val title: String,
     val description: String,
     val type: Type,
-    val createdAt: Instant
+    val createdAt: Instant = Clock.System.now(),
 ) {
     // Le voy a meter enums con campos extras para identificarlas mejor
     enum class Type { TEXT, AUDIO }
