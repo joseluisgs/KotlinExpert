247b16b2aa89e9b5eec189c5a590a379db22a438
José Luis González <joseluisgonsan@gmail.com>
2023-03-30 12:06:40 +0200

    Preparando SqlDeLight

--- Modulo06/MyNotes/build.gradle.kts
+++ Modulo06/MyNotes/build.gradle.kts
@@ -6,12 +6,14 @@ val datetime_version: String by project
 val logging_version: String by project
 val logback_version: String by project
 val coroutines_version: String by project
+val sqldelight_version: String by project
 
 // Inidicamos que es multiplataforma y compose
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
     kotlin("plugin.serialization") version "1.8.10"
+    id("app.cash.sqldelight")
 }
 
 group = "es.joseluisgs"
@@ -34,25 +36,35 @@ kotlin {
         val jvmMain by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
+
                 // Para iconos de la aplicación
                 implementation(compose.materialIconsExtended)
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:$datetime_version")
+
                 // Logger
                 implementation("io.github.microutils:kotlin-logging:$logging_version")
+
                 // Salida del logger
                 implementation("ch.qos.logback:logback-classic:$logback_version")
+
                 // Corrutinas
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$coroutines_version")
+
                 // Ktor client
                 implementation("io.ktor:ktor-client-core:$ktor_version")
                 implementation("io.ktor:ktor-client-okhttp:$ktor_version") // Engine
                 implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
+                // SQLDelight
+                implementation("app.cash.sqldelight:sqlite-driver:$sqldelight_version")
+                implementation("app.cash.sqldelight:coroutines-extensions:$sqldelight_version")
             }
         }
         val jvmTest by getting
     }
 }
+
 
 // Ejecutables de la aplicación destino
 compose.desktop {
@@ -62,6 +74,14 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "MyNotes"
             packageVersion = "1.0.0"
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("AppDatabase") {
+            packageName.set("database")
         }
     }
 }
--- Modulo06/MyNotes/settings.gradle.kts
+++ Modulo06/MyNotes/settings.gradle.kts
@@ -10,6 +10,7 @@ pluginManagement {
     plugins {
         kotlin("multiplatform").version(extra["kotlin.version"] as String)
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("app.cash.sqldelight").version(extra["sqldelight_version"] as String)
     }
 }
 
