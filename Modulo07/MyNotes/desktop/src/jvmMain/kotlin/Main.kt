87aacb751f8b90e4b999079f575623643ccb1923
José Luis González <joseluisgonsan@gmail.com>
2023-04-02 18:59:39 +0200

    Ajustes de Gradle y clean code

--- Modulo07/MyNotes/desktop/src/jvmMain/kotlin/Main.kt
+++ Modulo07/MyNotes/desktop/src/jvmMain/kotlin/Main.kt
@@ -3,7 +3,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import config.AppConfig
 import views.app.AppView
 
 
