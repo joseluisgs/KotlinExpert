f4fce3b8fef51e3630e775d6ec1b868a7ad772b0
José Luis González <joseluisgonsan@gmail.com>
2023-06-23 14:48:40 +0200

    Inicio mostrar lista

--- Modulo08/MyNotes/web/src/jsMain/kotlin/Main.kt
+++ Modulo08/MyNotes/web/src/jsMain/kotlin/Main.kt
@@ -1,12 +1,14 @@
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import org.jetbrains.compose.web.css.padding
-import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.dom.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import models.Note
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.H3
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
-import utils.getPlatformName
+import views.home.HomeViewModel
 
+/*
 fun main() {
 
     // Estados mutables
@@ -34,4 +36,63 @@ fun main() {
         }
     }
 }
+*/
+
+fun main() {
+    // Renderiza y podemos usar composables que se inyectan en el DOM en el elemento con id "root"
+    renderComposable(rootElementId = "root") {
+        console.log("Hola mundo")
+        // Crea un scope para usar coroutines
+        val scope = rememberCoroutineScope()
+        // Crea una instancia de HomeViewModel
+        val homeViewModel = remember { HomeViewModel(scope) }
+        // Composable que sea un listado de notas
+        //NoteList(homeViewModel.state.filterNotes ?: emptyList()) { note ->
+        // callback cuando se haga click
+        //console.log("Click en nota $note")
+        //}
+        homeViewModel.state.filterNotes?.forEach { note ->
+            Text(note.title)
+        }
+    }
+}
+
+@Composable
+fun NoteList(notes: List<Note>, onNoteClick: (Note) -> Unit) {
+    // Dentro del Div mostramos las notas
+    Div {
+        // Creamos un composable noteCard
+        notes.forEach { note ->
+            // NoteCard(note, onNoteClick)
+            Text(note.title)
+        }
+    }
+}
+
+@Composable
+fun NoteCard(note: Note, onNoteClick: (Note) -> Unit) {
+    // Div y maquetado de la nota
+    // Ahora los modificadores en Compose Desktop, en Compose Web son atributos
+    Div(
+        attrs = {
+            onClick { onNoteClick(note) }
+        }
+    ) {
+        // Título de la nota
+        H3 { Text(note.title) }
+        // Tipo de la nota
+        /* when (note.type) {
+             Note.Type.TEXT -> {
+                 Span { androidx.compose.material.Text("\uD83D\uDCC4") }
+             }
+
+             Note.Type.AUDIO -> {
+                 Span { androidx.compose.material.Text("\uD83C\uDFA4") }
+             }
+         }*/
+    }
+}
+
+
+
 
